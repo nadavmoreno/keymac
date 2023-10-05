d04d8162d95acac5d81451ee222479de0850ee2e
Nadav Moreno <nadavmoreno@gmail.com>
2023-10-05 11:29:43 +0300

    init

---
+++ keymac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  keymac
+//
+//  Created by Nadav Moreno on 05/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ keymac/keymacApp.swift
@@ -0,0 +1,17 @@
+//
+//  keymacApp.swift
+//  keymac
+//
+//  Created by Nadav Moreno on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct keymacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
